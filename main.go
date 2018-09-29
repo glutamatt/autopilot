d895f7135d3a14bf2650bf1fb7f771600809beb9
Glutamatt <glutamatt@live.fr>
2018-09-29 17:11:13 +0200

    Buggé mais ca avance

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"image/color"
 	"math"
-	"os"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/hajimehoshi/ebiten"
 )
 
-const minTurningRadius = 10
+const minTurningRadius = 20
+const slowDownDefault = .5
+const turnInc = .001
 
 //Position for Items
 type Position struct {
@@ -36,18 +34,28 @@ type Vehicule struct {
 //Drive a vehicule
 func (v *Vehicule) Drive(driving *Driving) {
 	v.Velocity += driving.Thrust
+	v.Velocity -= slowDownDefault
+	if v.Velocity < 0 {
+		v.Velocity = 0
+	}
 	turningRadius := GetTurningRadius(driving)
 	debug("turningRadius", turningRadius)
 	turningAngle := TurningAngle(v.Velocity, turningRadius)
 	debug("turningAngle", turningAngle)
 	debug("turningAngle Deg", turningAngle*180/math.Pi)
 	if turningAngle == 0 {
-		v.Position.x += math.Cos(v.Rotation) * v.Velocity
-		v.Position.y += math.Cos(math.Pi/2-v.Rotation) * v.Velocity
+		v.Position = Position{
+			x: v.x + math.Cos(v.Rotation)*v.Velocity,
+			y: v.y + math.Cos(math.Pi/2-v.Rotation)*v.Velocity,
+		}
 	} else {
 		v.Rotation += turningAngle
-		rotatePoint := RotatePoint(v.Position, turningRadius)
-		Rotate(&v.Position, turningAngle, rotatePoint)
+		targetRotate := Position{
+			x: v.x + math.Cos(v.Rotation)*turningRadius,
+			y: v.y + math.Cos(math.Pi/2-v.Rotation)*turningRadius,
+		}
+		Rotate(&targetRotate, math.Pi/-2, v.Position)
+		Rotate(&v.Position, turningAngle, targetRotate)
 	}
 }
 
@@ -61,21 +69,6 @@ func Rotate(point *Position, angle float64, around Position) {
 	yNew := point.x*s + point.y*c
 	point.x = xNew + around.x
 	point.y = yNew + around.y
-}
-
-//RotatePoint for turn
-func RotatePoint(pos Position, turningRadius float64) Position {
-	angle := 90 - math.Atan(pos.x/pos.y)
-	if turningRadius < 0 {
-		return Position{
-			x: pos.x + math.Sin(angle)*turningRadius,
-			y: pos.y - math.Cos(angle)*turningRadius,
-		}
-	}
-	return Position{
-		x: pos.x - math.Sin(angle)*turningRadius,
-		y: pos.y + math.Cos(angle)*turningRadius,
-	}
 }
 
 //TurningAngle from velocity and turningRadius
@@ -107,61 +100,62 @@ var square *ebiten.Image
 
 func main() {
 
-	v := Vehicule{}
-	drives := make(chan Driving)
+	v := Vehicule{Position: Position{y: 100}}
+	drive := &Driving{}
 
 	update := func(screen *ebiten.Image) error {
-		screen.Fill(color.NRGBA{0xff, 0x00, 0x00, 0xff})
 
+		println("----------------")
+
+		if ebiten.IsKeyPressed(ebiten.KeyUp) {
+			if drive.Thrust == 0 {
+				drive.Thrust = 1
+			}
+			drive.Thrust += 1 / drive.Thrust
+		} else {
+			drive.Thrust = 0
+		}
+
+		if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+			if drive.Turning < 1 {
+				if drive.Turning < 0 {
+					drive.Turning = 0
+				}
+				drive.Turning += turnInc
+			}
+		} else {
+			if ebiten.IsKeyPressed(ebiten.KeyRight) {
+				if drive.Turning > -1 {
+					if drive.Turning > 0 {
+						drive.Turning = 0
+					}
+					drive.Turning -= turnInc
+				}
+			} else {
+				drive.Turning = 0
+			}
+		}
+
+		debug("drive.Turning", drive.Turning)
+
+		screen.Fill(color.NRGBA{0xff, 0x00, 0x00, 0xff})
 		if square == nil {
 			square, _ = ebiten.NewImage(3, 3, ebiten.FilterNearest)
 		}
 		square.Fill(color.White)
 
 		opts := &ebiten.DrawImageOptions{}
-
-		select {
-		case d := <-drives:
-			v.Drive(&d)
-		default:
-			v.Drive(&Driving{})
-		}
-
-		debug("v", fmt.Sprintf("rot: %.1f", v.Rotation))
-
+		v.Drive(drive)
+		debug("v.Rotation", fmt.Sprintf("rot: %.3f", v.Rotation))
 		scale := 10.0
 		opts.GeoM.Reset()
 		opts.GeoM.Translate(v.x/scale, v.y/scale)
 		screen.DrawImage(square, opts)
 
-		time.Sleep(50 * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 
 		return nil
 	}
-
-	go func() {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			in := scanner.Text()
-			if in != "" {
-				d := Driving{}
-				pieces := strings.Split(in, " ")
-				turning, err := strconv.ParseFloat(pieces[0], 64)
-				if err != nil {
-					panic(err)
-				}
-				d.Turning = turning
-				if len(pieces) > 1 {
-					thurst, err := strconv.ParseFloat(pieces[1], 64)
-					if err != nil {
-						panic(err)
-					}
-					d.Thrust = thurst
-				}
-				drives <- d
-			}
-		}
-	}()
 
 	if err := ebiten.Run(update, 500, 500, 2, "Hello world!"); err != nil {
 		panic(err)
