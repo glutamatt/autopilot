4e20df115574d7e263a45e29763e8362f801c271
Glutamatt <glutamatt@live.fr>
2018-10-02 09:57:54 +0200

    Optim // computing YEAH Smoooooth

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"image/color"
 	"math"
 	"math/rand"
-	"time"
+	"sync"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
@@ -92,8 +92,9 @@ func createRandomVehicule() *Vehicule {
 }
 
 func main() {
-	vehicules := make([]*Vehicule, 30)
+	vehicules := make([]*Vehicule, 500)
 	vehiculeImage, _ := ebiten.NewImage(int(carWidth*uiScale), int(carHeight*uiScale), ebiten.FilterNearest)
+	vehiculeImage.Fill(color.White)
 	for i := range vehicules {
 		vehicules[i] = createRandomVehicule()
 	}
@@ -104,36 +105,37 @@ func main() {
 		inputControls(drive)
 		screen.Fill(color.NRGBA{0x88, 0x00, 0x00, 0xff})
 
+		wg := sync.WaitGroup{}
+		wg.Add(len(vehicules))
+		optsChan := make(chan ebiten.DrawImageOptions)
+
 		for _, v := range vehicules {
-			v.Drive(drive, 1.0/60)
-			opts := ebiten.DrawImageOptions{}
-			opts.GeoM.Translate(carWidth*uiScale/-4, carHeight*uiScale/-2)
-			opts.GeoM.Rotate(v.Rotation * -1)
-			opts.GeoM.Translate(carWidth*uiScale/4, carHeight*uiScale/2)
-			opts.GeoM.Translate(v.x*uiScale, v.y*uiScale*-1+uiHeight)
-			vehiculeImage.Fill(color.White)
-			screen.DrawImage(vehiculeImage, &opts)
+			go func(v *Vehicule) {
+				v.Drive(drive, 1.0/60)
+				opts := ebiten.DrawImageOptions{}
+				opts.GeoM.Translate(carWidth*uiScale/-4, carHeight*uiScale/-2)
+				opts.GeoM.Rotate(v.Rotation * -1)
+				opts.GeoM.Translate(carWidth*uiScale/4, carHeight*uiScale/2)
+				opts.GeoM.Translate(v.x*uiScale, v.y*uiScale*-1+uiHeight)
+				optsChan <- opts
+				wg.Done()
+			}(v)
 		}
+
+		go func() {
+			for opts := range optsChan {
+				screen.DrawImage(vehiculeImage, &opts)
+			}
+		}()
+
+		wg.Wait()
+		close(optsChan)
 
 		ebitenutil.DebugPrint(screen, fmt.Sprintf(
 			"%#v\n%.1f km/h\n%.2f°\nfps:%.0f\nvcount: %d",
 			drive, vehicules[0].Velocity/1000*60*60, vehicules[0].Rotation*180/math.Pi, ebiten.CurrentFPS(), len(vehicules)))
 		return nil
 	}
-
-	go func() {
-		for {
-			time.Sleep(time.Millisecond * 300)
-			if ebiten.CurrentFPS() > 58 {
-				v := createRandomVehicule()
-				v.Velocity = vehicules[0].Velocity
-				vehicules = append(vehicules, v)
-			}
-			if ebiten.CurrentFPS() < 55 {
-				vehicules = vehicules[1:]
-			}
-		}
-	}()
 
 	if err := ebiten.Run(update, uiWidth, uiHeight, 2, "Hello world!"); err != nil {
 		panic(err)
