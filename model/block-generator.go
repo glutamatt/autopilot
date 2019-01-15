2811815e7daa0bfefff576500c4088d360f3099e
Glutamatt <glutamatt@live.fr>
2019-01-15 18:38:01 +0100

    improve block generation to let large enough roads

--- model/block-generator.go
+++ model/block-generator.go
@@ -7,11 +7,12 @@ import (
 
 func GenerateBlocks(w, h float64) []*Position {
 	rSource := rand.New(rand.NewSource(time.Now().Unix()))
-
-	blockCnt := 140
+	blockCnt := 200
+	forbidDistance := float64(5 * BlockBorder)
 	positions := []*Position{}
+	forbidenBlocks := map[Position]bool{}
 	for i := 0; i < blockCnt; i++ {
-		bw, bh := rSource.Float64()*20+5, rSource.Float64()*20+5
+		bw, bh := rSource.Float64()*50+15, rSource.Float64()*50+15
 		bStart := &Position{rSource.Float64() * w, rSource.Float64() * -h}
 		wBCount, hBCount := int(bw)/BlockBorder, int(bh)/BlockBorder
 
@@ -19,8 +20,21 @@ func GenerateBlocks(w, h float64) []*Position {
 			for wi := 0; wi <= wBCount; wi++ {
 				pos := &Position{bStart.X - bw/2 + float64(wi*BlockBorder), bStart.Y + bh/2 - float64(hi*BlockBorder)}
 				pos.Gap(BlockBorder)
+				if forbiden := forbidenBlocks[*pos]; forbiden {
+					continue
+				}
 				positions = append(positions, pos)
 			}
+		}
+		forbidPos := Position{bStart.X - bw/2 - forbidDistance, bStart.Y - bh/2 - forbidDistance}
+		for forbidPos.X < bStart.X+bw/2+forbidDistance {
+			for forbidPos.Y < bStart.Y+bh/2+forbidDistance {
+				forbidPos.Gap(BlockBorder)
+				forbidenBlocks[forbidPos] = true
+				forbidPos.Y += float64(BlockBorder)
+			}
+			forbidPos.Y = bStart.Y - bh/2 - forbidDistance
+			forbidPos.X += float64(BlockBorder)
 		}
 	}
 
