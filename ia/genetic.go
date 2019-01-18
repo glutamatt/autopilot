23052be26b04e3ff35013044aff0710dc519bcbe
Glutamatt <glutamatt@live.fr>
2019-01-18 12:20:33 +0100

    WorkingToOptimizeThough

--- ia/genetic.go
+++ ia/genetic.go
@@ -3,36 +3,94 @@ package ia
 import (
 	"math"
 	"math/rand"
+	"sort"
+	"sync"
 	"time"
 
 	"github.com/glutamatt/autopilot/model"
 )
 
-var driveSequenceLen = 2
+var driveSequenceLen = 5
 var intervalTime = 500 * time.Millisecond
 var random = rand.New(rand.NewSource(time.Now().UnixNano()))
 var VehiculRadius float64
 var BlocRadius float64
 
 func Genetic(vehicule *model.Vehicule, path *[]model.Position, blocks *map[model.Position]bool) *model.Driving {
-	var bestSeq *sequence
 	filteredBlocks := filterBlocks(vehicule.Position, blocks)
-	for i := 0; i < 1000; i++ {
-		seq := sequence{
-			drives: driveSequence(),
-			vehicule: model.Vehicule{
-				Position: vehicule.Position,
-				Rotation: vehicule.Rotation,
-				Velocity: vehicule.Velocity,
-			},
-		}
-		seq.compute(intervalTime, filteredBlocks, path)
-		if bestSeq == nil || bestSeq.cost > seq.cost {
-			bestSeq = &seq
+
+	sequences := generateSequences(1000, vehicule)
+	computeSequences(&sequences, filteredBlocks, path)
+	sort.Slice(sequences, func(i, j int) bool { return sequences[i].cost < sequences[j].cost })
+
+	timer := time.NewTimer(time.Second/60 - 10*time.Millisecond)
+	keepComputing := true
+	for keepComputing {
+		sequences = sequences[:10]
+		sequences = append(sequences, crossOver(10, &sequences, vehicule)...)
+		sequences = append(sequences, mutateSequences(5, &sequences, vehicule)...)
+		computeSequences(&sequences, filteredBlocks, path)
+		sort.Slice(sequences, func(i, j int) bool { return sequences[i].cost < sequences[j].cost })
+
+		select {
+		case <-timer.C:
+			keepComputing = false
+		default:
 		}
 	}
 
-	return bestSeq.drives[0]
+	return sequences[0].drives[0]
+}
+
+func mutateSequences(crossedLen int, sequences *[]*sequence, vehicule *model.Vehicule) []*sequence {
+	crossed := make([]*sequence, crossedLen)
+	sequencesLen := len(*sequences)
+	for i := 0; i < crossedLen; i++ {
+		crossed[i] = &sequence{
+			drives:   make([]*model.Driving, driveSequenceLen),
+			vehicule: copyVehicule(vehicule),
+		}
+		copy(crossed[i].drives, (*sequences)[i%sequencesLen].drives)
+		crossed[i].drives[random.Intn(driveSequenceLen)] = gene()
+	}
+	return crossed
+}
+
+func crossOver(crossedLen int, sequences *[]*sequence, vehicule *model.Vehicule) []*sequence {
+	crossed := make([]*sequence, crossedLen)
+	sequencesLen := len(*sequences)
+	for i := 0; i < crossedLen; i++ {
+		fatherLen := random.Intn(driveSequenceLen)
+		crossed[i] = &sequence{
+			drives:   append((*sequences)[i%sequencesLen].drives[:fatherLen], (*sequences)[random.Intn(sequencesLen)].drives[fatherLen:]...),
+			vehicule: copyVehicule(vehicule)}
+	}
+	return crossed
+}
+
+func copyVehicule(vehicule *model.Vehicule) model.Vehicule {
+	return model.Vehicule{
+		Position: vehicule.Position,
+		Rotation: vehicule.Rotation,
+		Velocity: vehicule.Velocity,
+	}
+}
+
+func generateSequences(len int, vehicule *model.Vehicule) []*sequence {
+	sequences := make([]*sequence, len)
+	for i := range sequences {
+		sequences[i] = &sequence{drives: driveSequence(), vehicule: copyVehicule(vehicule)}
+	}
+	return sequences
+}
+
+func computeSequences(sequences *[]*sequence, filteredBlocks *[]model.Position, path *[]model.Position) {
+	wg := sync.WaitGroup{}
+	wg.Add(len(*sequences))
+	for _, seq := range *sequences {
+		go func(s *sequence) { s.compute(intervalTime, filteredBlocks, path); wg.Done() }(seq)
+	}
+	wg.Wait()
 }
 
 func filterBlocks(vehicule model.Position, blocks *map[model.Position]bool) *[]model.Position {
